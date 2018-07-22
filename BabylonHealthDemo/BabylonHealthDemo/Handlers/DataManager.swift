50dc8144d877005832b43f0ed4568ec86e18b534
Benjamin <ben@henshall.plus.com>
2018-07-22 23:28:49 +0100

    refactor(*): moved protocols into their own files/group

--- BabylonHealthDemo/BabylonHealthDemo/Handlers/DataManager.swift
+++ BabylonHealthDemo/BabylonHealthDemo/Handlers/DataManager.swift
@@ -1,70 +1,14 @@
 import RxSwift
 
-protocol DataManagerType {
-  
-  // MARK: Post Retrieval
-
-  /// Retrieves all posts
-  ///
-  /// - Returns: Observable that emits persistent posts, then fresh posts, then completes
-  func posts() -> Observable<[Post]>
-  
-  /// Retrieves X posts starting from a given ID
-  ///
-  /// - Parameters:
-  ///   - startingID: The ID of the first post
-  ///   - limit: The number of posts to return
-  /// - Returns: Observable that emits persistent posts, then fresh posts, then completes
-  func posts(startingFrom startingID: Int64, limit: Int64) -> Observable<[Post]>
-  
-  /// Retrieves a post matching a given ID
-  ///
-  /// - Parameter id: The identifier of the post to return
-  /// - Returns: Observable that emits the persistent post, then the fresh post, then completes
-  func post(id: Int64) -> Observable<[Post]>
-  
-  // MARK: User Retrieval
-
-  /// Retrieves all users
-  ///
-  /// - Returns: Observable that emits persistent users, then fresh users, then completes
-  func users() -> Observable<[User]>
-
-  /// Retrieves a user matching a given ID
-  ///
-  /// - Parameter id: The identifier of the user to return
-  /// - Returns: Observable that emits the persistent user, then the fresh user, then completes
-  func user(id: Int64) -> Observable<[User]>
-  
-  // MARK: Comment Retrieval
-  
-  /// Retrieves all comments
-  ///
-  /// - Returns: Observable that emits persistent comments, then fresh comments, then completes
-  func comments() -> Observable<[Comment]>
-  
-  /// Retrieves a comment matching a given ID
-  ///
-  /// - Parameter id: The identifier of the comment to return
-  /// - Returns: Observable that emits the persistent comment, then the fresh comment, then completes
-  func comment(id: Int64) -> Observable<[Comment]>
-  
-  /// Retrieves all comments on a given post
-  ///
-  /// - Parameter id: The identifier of the post to retrieve comments for
-  /// - Returns: Observable that emits the persistent comments on a post, then the fresh comments, then completes
-  func comments(on postID: Int64) -> Observable<[Comment]>
-}
-
 /// Manager class that manages both the persistence of data and the pulling of new data through
 /// different services
 class DataManager: DataManagerType {
   private let disposeBag = DisposeBag()
   
-  private let apiService: APIServiceType
-  private let persistenceManager: PersistenceManagerType
+  private let apiService: NetworkService
+  private let persistenceManager: PersistenceManager
   
-  init(apiService: APIServiceType, persistenceManager: PersistenceManagerType) {
+  init(apiService: NetworkService, persistenceManager: PersistenceManager) {
     self.apiService = apiService
     self.persistenceManager = persistenceManager
   }
--- BabylonHealthDemo/BabylonHealthDemo/Handlers/NetworkHandler.swift
+++ BabylonHealthDemo/BabylonHealthDemo/Handlers/NetworkHandler.swift
@@ -3,63 +3,7 @@ import RealmSwift
 import RxSwift
 import RxRealm
 
-protocol APIServiceType {
-  
-  // MARK: Post fetching
-  
-  /// Fetches all posts from the API
-  ///
-  /// - Returns: A Single that emits an array of posts then completes
-  func posts() -> Single<[Post]>
-  
-  /// Retrieves X Post objects starting from a given ID
-  ///
-  /// - Parameters:
-  ///   - startingID: The ID of the first post
-  ///   - limit: The number of posts to return
-  /// - Returns: Single that emits posts pulled from an API
-  func posts(startingFrom startingID: Int64, limit: Int64) -> Single<[Post]>
-  
-  /// Fetches a specific post object when given an ID
-  ///
-  /// - Parameter id: The identifier of the post to fetch
-  /// - Returns: A Single emitting an array of size 1, containing the requested post
-  func post(id: Int64) -> Single<[Post]>
-  
-  // MARK: User Fetching
-  
-  /// Fetches all users from the API
-  ///
-  /// - Returns: A Single that emits an array of users then completes
-  func users() -> Single<[User]>
-
-  /// Fetches a specific user object when given an ID
-  ///
-  /// - Parameter id: The identifier of the user to fetch
-  /// - Returns: A Single emitting an array of size 1, containing the requested user
-  func user(id: Int64) -> Single<[User]>
-  
-  // MARK: Comment Fetching
-  
-  /// Fetches all comments from the API
-  ///
-  /// - Returns: A Single that emits an array of comments then completes
-  func comments() -> Single<[Comment]>
-  
-  /// Fetches a specific Comment object when given an ID
-  ///
-  /// - Parameter id: The identifier of the Comment to fetch
-  /// - Returns: A Single emitting an array of size 1, containing the requested Comment
-  func comment(id: Int64) -> Single<[Comment]>
-  
-  /// Fetches all Comments with a matching postID form the API
-  ///
-  /// - Parameter postID: The identifier of the post to fetch comments for
-  /// - Returns: A Single emitting an array of all comments with a matching postID
-  func comments(on postID: Int64) -> Single<[Comment]>
-}
-
-class APIService: APIServiceType {
+class APIService: NetworkService {
   
   fileprivate enum Endpoint: String {
     case posts
