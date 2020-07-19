99f044b2fa394c7054f2faf500a842248d1138c2
Benjamin <ben@henshall.plus.com>
2020-07-19 15:25:47 +0100

    Got project to compile with updated libraries

--- BabylonHealthDemo/BabylonHealthDemo/AppDelegate.swift
+++ BabylonHealthDemo/BabylonHealthDemo/AppDelegate.swift
@@ -41,8 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
 
   private func setupLogging() {
-    DDLog.add(DDTTYLogger.sharedInstance) // TTY = Xcode console
-    DDLog.add(DDASLLogger.sharedInstance) // ASL = Apple System Logs
+    DDLog.add(DDOSLogger.sharedInstance) // ASL = Apple System Logs
 
     let fileLogger: DDFileLogger = DDFileLogger() // File Logger
     fileLogger.rollingFrequency = TimeInterval(60*60*24)  // 24 hours
